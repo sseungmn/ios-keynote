9eab4efeb37a9c2f2ee1542f4114b6a40e1f884a
miniOS <seungmin.oh.dev@gmail.com>
2023-07-17 10:33:18 +0900

    chore: 프로젝트 생성

---
+++ MyKeynote/MyKeynote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyKeynote
+//
+//  Created by SEUNGMIN OH on 2023/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
