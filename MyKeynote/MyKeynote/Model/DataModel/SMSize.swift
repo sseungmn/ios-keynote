78222b951b3996495fdd9460d12755f88256ba7c
miniOS <seungmin.oh.dev@gmail.com>
2023-07-17 14:03:32 +0900

    feat: 고유 ratio를 갖는 SMSize 데이터 모델 정의

---
+++ MyKeynote/MyKeynote/Model/DataModel/SMSize.swift
@@ -0,0 +1,27 @@
+//
+//  SMSize.swift
+//  MyKeynote
+//
+//  Created by SEUNGMIN OH on 2023/07/17.
+//
+
+import Foundation
+
+struct SMSize {
+    private var ratio: CGFloat
+
+    var width: Int
+    var height: Int {
+        return Int(CGFloat(width) * ratio)
+    }
+
+    init(width: Int, ratio: CGFloat) {
+        self.width = width
+        self.ratio = ratio
+    }
+
+    init(ratio: CGFloat, using generator: inout RandomNumberGenerator) {
+        self.width = Int.random(in: 0..<300, using: &generator)
+        self.ratio = ratio
+    }
+}
