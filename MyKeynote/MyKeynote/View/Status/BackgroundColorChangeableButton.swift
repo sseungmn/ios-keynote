6ac30752c06b8194585f5b073d39bf633d0462a4
miniOS <seungmin.oh.dev@gmail.com>
2023-07-20 02:07:00 +0900

    feat: BackgroundColor가 바뀌며 hexCode로 표현해주는 button 구현

---
+++ MyKeynote/MyKeynote/View/Status/BackgroundColorChangeableButton.swift
@@ -0,0 +1,32 @@
+//
+//  BackgroundColorChangeableButton.swift
+//  MyKeynote
+//
+//  Created by SEUNGMIN OH on 2023/07/20.
+//
+
+import UIKit
+
+class BackgroundColorChangeableButton: UIButton {
+    override var backgroundColor: UIColor? {
+        willSet {
+            guard let newValue else { return }
+            setTitleColor(newValue.complementaryColor(), for: .normal)
+            setTitle(newValue.hexString, for: .normal)
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
+    private func configureUI() {
+        layer.cornerRadius = 5.0
+    }
+}
