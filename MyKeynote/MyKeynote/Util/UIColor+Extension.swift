1cb792c9c96fe9bf0e2bb8c7cbd1e9a992c398e1
miniOS <seungmin.oh.dev@gmail.com>
2023-07-19 19:04:32 +0900

    feat: 보색을 구해주는 메서드 구현

---
+++ MyKeynote/MyKeynote/Util/UIColor+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extension.swift
+//  MyKeynote
+//
+//  Created by SEUNGMIN OH on 2023/07/19.
+//
+
+import UIKit
+
+extension UIColor {
+    // https://gist.github.com/klein-artur/025a0fa4f167a648d9ea
+    func complementaryColor() -> UIColor {
+        let ciColor = CIColor(color: self)
+
+        let compRed: CGFloat = 1.0 - ciColor.red
+        let compGreen: CGFloat = 1.0 - ciColor.green
+        let compBlue: CGFloat = 1.0 - ciColor.blue
+
+        // https://stackoverflow.com/questions/1496716/how-do-i-get-red-green-blue-rgb-and-alpha-back-from-a-uicolor-object
+        var alpha: CGFloat = 0.0
+        getRed(nil, green: nil, blue: nil, alpha: &alpha)
+
+        return UIColor(red: compRed, green: compGreen, blue: compBlue, alpha: alpha)
+    }
+}
