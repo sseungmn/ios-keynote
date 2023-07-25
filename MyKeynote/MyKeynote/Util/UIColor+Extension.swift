9fd871bd01d7eae3364a403f23f5e80f3d744c1c
miniOS <seungmin.oh.dev@gmail.com>
2023-07-25 16:56:29 +0900

    fix: SMColor에서 SMAlpha를 분리

--- MyKeynote/MyKeynote/Util/UIColor+Extension.swift
+++ MyKeynote/MyKeynote/Util/UIColor+Extension.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 extension UIColor {
-    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+    typealias RGB = (red: CGFloat, green: CGFloat, blue: CGFloat)
+    
+    var rgb: RGB {
         let ciColor = CIColor(color: self)
 
-        var alpha: CGFloat = 0.0
-        getRed(nil, green: nil, blue: nil, alpha: &alpha)
-
-        return (red: ciColor.red, green: ciColor.green, blue: ciColor.blue, alpha: ciColor.alpha)
+        return (red: ciColor.red, green: ciColor.green, blue: ciColor.blue)
     }
 
     // https://stackoverflow.com/a/58158413/19782341
@@ -28,16 +27,16 @@ extension UIColor {
 
     // https://stackoverflow.com/a/596243/19782341
     var isDark: Bool {
-        let rgba = self.rgba
+        let rgb = self.rgb
 
-        let luminance = 0.299 * rgba.red + 0.587 * rgba.green + 0.114 * rgba.blue
+        let luminance = 0.299 * rgb.red + 0.587 * rgb.green + 0.114 * rgb.blue
         return luminance < 0.5
     }
 
     // https://gist.github.com/klein-artur/025a0fa4f167a648d9ea
     func complementaryColor() -> UIColor {
-        let rgba = self.rgba
+        let rgb = self.rgb
 
-        return UIColor(red: 1 - rgba.red, green: 1 - rgba.green, blue: 1 - rgba.blue, alpha: rgba.alpha)
+        return UIColor(red: 1 - rgb.red, green: 1 - rgb.green, blue: 1 - rgb.blue, alpha: 1.0)
     }
 }
